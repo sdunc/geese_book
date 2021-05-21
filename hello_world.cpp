2769553bd758f7d273f80703782db2ca570bd286
Stephen Duncanson <stephen.duncanson@gmail.com>
2021-05-21 09:42:20 -0400

    first example program!

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+// This program outputs the message "Hello, World!" to the monitor
+
+#include "std_lib_facilities.h"
+
+int main() // c++ programs start by executing the function main 
+{
+  cout << "Hello, World!"; // output "Hello, World!"
+  return 0;
+}
+
