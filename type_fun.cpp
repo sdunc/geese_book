855bcadf6f7f7d9d41af83b4323e3460f11f6b99
Stephen Duncanson <Stephen.duncanson@gmail.com>
2021-06-11 15:13:33 -0400

    page 69

---
+++ type_fun.cpp
@@ -0,0 +1,19 @@
+#include "std_lib_facilities.h"
+
+int main() {
+  // stored objects have a name and a type
+  // the following shows the defintion of several variables of various types.
+  string name = "Annemaria";
+  int number_of_steps = 39;
+  // c++ has a lot of types but "you can write perfectly good programs using only 5:
+  double flying_time = 3.5;
+  char decimal_point = '.';
+  bool tap_on = true;
+  // int: an integer
+  // double: a floating-point number
+  // char: a single character enclosed in single quotes
+  // string: a sequence of characters delimited by double quotes
+  // bool: a Boolean, true or false
+}
+
+
