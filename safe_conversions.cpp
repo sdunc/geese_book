82b767f6ec177bacb04404aae210469bc28818ed
Stephen Duncanson <Stephen.duncanson@gmail.com>
2021-06-17 14:34:52 -0400

    finishing chapter 3

---
+++ safe_conversions.cpp
@@ -0,0 +1,10 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+  char c ='x';
+  int i1 = c;
+  int i2 = 'x';
+  char c2 = i1;
+  cout << c << ' ' << i1 << ' ' << c2 << '\n';
+}
---
+++ while_int_char.cpp
@@ -0,0 +1,16 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+  double d = 0;
+  while (cin>>d) {
+    // repeat the statements below
+    int i = d;
+    char c = i;
+    int i2 = c;
+    cout << " d==" << d
+	 << " i==" << i
+	 << " i2==" << i2
+	 << " char(" << c << ")\n";
+  }
+}
